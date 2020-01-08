6b5941221d6d1d666b5eeeee388646cb16e3cf98
Ruan Pablo Medeiros <pm.ruan@gmail.com>
2020-01-08 19:30:57 -0300

    page

---
+++ precache-manifest.2ed6a145a802695c3dcb4352a354bdd1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c618b2b41fd31b7e0fc8fa0693004c63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "843c54517df2ff712980",
+    "url": "/static/css/main.350c4287.chunk.css"
+  },
+  {
+    "revision": "bee5958802d0a5f5469c",
+    "url": "/static/js/2.39a5c22d.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/static/js/2.39a5c22d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "843c54517df2ff712980",
+    "url": "/static/js/main.3be20a97.chunk.js"
+  },
+  {
+    "revision": "613363f8c33d428b1d72",
+    "url": "/static/js/runtime-main.45b56331.js"
+  },
+  {
+    "revision": "31ba3e2f6186b99ed1f4f665b51b0053",
+    "url": "/static/media/code-bg.31ba3e2f.png"
+  },
+  {
+    "revision": "96aab6ab862673b417b730a9ced72623",
+    "url": "/static/media/left-bracket.96aab6ab.png"
+  },
+  {
+    "revision": "396fcb75d61341aab3aba75bb8660b17",
+    "url": "/static/media/notebook.396fcb75.svg"
+  },
+  {
+    "revision": "5f69aa743946f5488734b26a1e00a7cf",
+    "url": "/static/media/right-bracket.5f69aa74.png"
+  }
+]);
